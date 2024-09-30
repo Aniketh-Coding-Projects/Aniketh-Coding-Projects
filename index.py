548139805fd83e1872a47468bd07837c8dcf9f98
Aniketh-Coding-Projects <aniketh.pamu@stu.ocsb.ca>
2024-09-30 11:43:49 -0400

    index.py

---
+++ index.py
@@ -0,0 +1,18 @@
+print("hello,world")
+x=people
+head=1*x
+legs=2*x
+arms=x*2
+toes=10*x
+x = x + y
+y = x - y
+x = x - y
+print(1+ 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
+print("hello")
+username = "joe"
+print(username)
+meatPrice = 4.00
+meatTax = 0.03 * meatPrice
+milkPrice = 2.00
+milkTax = 0.03 * milkPrice
+print(meatPrice + meatTax + milkPrice + milkTax)
